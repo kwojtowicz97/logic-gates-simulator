268cbf8bd86e8af586b0f556198061a375aa9641
kwojtowicz97 <kamilwojtowicz079@gmail.com>
2023-01-08 21:52:43 +0100

    Added components icons

--- src/Sidebar/Sidebar.tsx
+++ src/Sidebar/Sidebar.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { TBlocks } from '../App'
+import { gates } from '../Nodes/logic'
+import './Sidebar.css'
 
 type TSidebarProps = {
   addBlock: () => void
@@ -23,90 +25,120 @@ const Sidebar = ({ addBlock, blocks }: TSidebarProps) => {
   const addBlockHandler = () => addBlock()
 
   return (
-    <div style={{ width: '20%', borderRight: '1px solid black' }}>
+    <div className='sidebar-components-menu'>
       <div
-        className='dndnode'
+        className='sidebar-component'
         onDragStart={(event) => onDragStart(event, 'in')}
         draggable
       >
-        INPUT
+        <div className='circle'>
+          <div style={{ translate: 'translateX(-45%)' }}>In</div>
+        </div>
+        <div style={{ marginTop: '4px' }}>INPUT</div>
       </div>
       <div
-        className='dndnode'
+        className='sidebar-component'
+        onDragStart={(event) => onDragStart(event, 'out')}
+        draggable
+      >
+        <div className='circle'>
+          <div>Out</div>
+        </div>
+        <div style={{ marginTop: '4px' }}>INPUT</div>
+      </div>
+      <div
+        className='sidebar-component'
         onDragStart={(event) => onDragStart(event, 'or')}
         draggable
       >
-        OR
+        <img alt='and gate' src={gates.or.image} />
+        <div>OR</div>
       </div>
       <div
-        className='dndnode'
+        className='sidebar-component'
         onDragStart={(event) => onDragStart(event, 'and')}
         draggable
       >
-        AND
+        <img alt='and gate' src={gates.and.image} />
+        <div>AND</div>
       </div>
       <div
-        className='dndnode'
+        className='sidebar-component'
         onDragStart={(event) => onDragStart(event, 'xor')}
         draggable
       >
-        XOR
+        <img alt='xor gate' src={gates.xor.image} />
+        <div>XOR</div>
       </div>
       <div
-        className='dndnode'
+        className='sidebar-component'
         onDragStart={(event) => onDragStart(event, 'nand')}
         draggable
       >
-        NAND
+        <img alt='nand gate' src={gates.nand.image} />
+        <div>NAND</div>
       </div>
       <div
-        className='dndnode'
+        className='sidebar-component'
         onDragStart={(event) => onDragStart(event, 'nor')}
         draggable
       >
-        NOR
+        <img alt='nor gate' src={gates.nor.image} />
+        <div>NOR</div>
       </div>
       <div
-        className='dndnode'
+        className='sidebar-component'
         onDragStart={(event) => onDragStart(event, 'xnor')}
         draggable
       >
-        XNOR
+        <img alt='xnor gate' src={gates.xnor.image} />
+        <div>XNOR</div>
       </div>
       <div
-        className='dndnode'
+        className='sidebar-component'
         onDragStart={(event) => onDragStart(event, 'not')}
         draggable
       >
-        NOT
+        <img alt='not gate' src={gates.not.image} />
+        <div>NOT</div>
       </div>
+
       <div
-        className='dndnode'
-        onDragStart={(event) => onDragStart(event, 'clk')}
-        draggable
-      >
-        CLOCK
-      </div>
-      <div
-        className='dndnode'
-        onDragStart={(event) => onDragStart(event, 'blockInput')}
-        draggable
-      >
-        Block Input
-      </div>
-      <div
-        className='dndnode'
-        onDragStart={(event) => onDragStart(event, 'blockOutput')}
-        draggable
-      >
-        Block Output
-      </div>
-      <div
-        className='dndnode'
+        className='sidebar-component'
+        style={{ paddingTop: '5%' }}
         onDragStart={(event) => onDragStart(event, 'display')}
         draggable
       >
-        Display
+        <img
+          style={{ height: '50px', width: 'auto' }}
+          alt='display'
+          src='./assets/display.png'
+        />
+        <div>DISPLAY</div>
+      </div>
+      <div
+        className='sidebar-component'
+        onDragStart={(event) => onDragStart(event, 'blockInput')}
+        draggable
+      >
+        <img
+          style={{ height: '35px', width: 'auto' }}
+          alt='display'
+          src='./assets/block_input.png'
+        />
+        <div style={{ maxWidth: '70%', margin: 'auto' }}>BLOCK INPUT</div>
+      </div>
+      <div
+        className='sidebar-component'
+        onDragStart={(event) => onDragStart(event, 'blockOutput')}
+        draggable
+      >
+        <img
+          style={{ height: '35px', width: 'auto' }}
+          alt='display'
+          src='./assets/block_output.png'
+        />
+        <div style={{ maxWidth: '70%', margin: 'auto' }}>BLOCK OUTPUT</div>
       </div>
       <button onClick={addBlockHandler}>Add block</button>
       {blocks.map((block) => (
